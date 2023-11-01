138ec898cc5ed5f8dfb0879ecb5f58018e29803f
QuirrelForU <symmlux@gmail.com>
2023-11-01 14:34:42 +0700

    new search path alghoritm + fixed path saving

--- fasdfasf.py
+++ fasdfasf.py
@@ -1,18 +1,6 @@
 import copy
 import random
 
-
-def print_matrix(matrix):
-    # Находим максимальную длину элемента в матрице для выравнивания
-    max_length = max(len(str(element)) for row in matrix for element in row)
-
-    for row in matrix:
-        for element in row:
-            # Выводим элементы с выравниванием
-            print(f"{element:{max_length}}", end="  ")
-        print()  # Переходим на новую строку после каждой строки матрицы
-
-
 # не проверяют индексы справа
 rows = 7
 cols = 7
@@ -78,9 +66,6 @@ for i in range(1, cols_fixed - 2, 2):
             if a[rows_fixed - 2][z] == temp:
                 a[rows_fixed - 2][z] == a[rows_fixed - 2][i]
 
-# print('indeciesss before fixe')
-# print_matrix(a)
-
 # delete all trash-numbers
 for i in range(1, rows_fixed, 2):
     for j in range(1, cols_fixed, 2):
@@ -137,38 +122,6 @@ def maze_print_solved(maze, cols_fixed, rows_fixed, path):
         print()
 
 
-print('generated_maze')
-maze_print(a, cols_fixed, rows_fixed)
-
-created_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-                [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1],
-                [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
-                [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-                [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-                [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-
-print('\nstable_maze ')
-maze_print(created_maze, cols_fixed, rows_fixed)
-print('\n\n\n')
-
-# start and end in fixed format rows, cols
-start = (1, 1)
-end = (13, 13)
-solving_maze = copy.copy(created_maze)
-for i in range(1, rows_fixed, 2):
-    for j in range(1, cols_fixed, 2):
-        solving_maze[i][j] = [0, 0]
-
-
 def shortest_distance(start_pos, end_pos):
     return (abs(start_pos[0] - end_pos[0]) + abs(start_pos[1] - end_pos[1])) // 2
 
@@ -181,14 +134,15 @@ def find_element_in_matrix(matrix, target):
     return None  # Если элемент не найден, вернуть None
 
 
-def a_way_out(maze, start_pos, end_pos, right_path):
+def a_way_out(maze, start_pos, end_pos):
     maze[start_pos[0]][start_pos[1]][1] = 1
 
     def try_move(direction, distance):
         new_pos_wall = (start_pos[0] + direction[0], start_pos[1] + direction[1])
         new_pos = (start_pos[0] + direction[0] * distance, start_pos[1] + direction[1] * distance)
         if maze[new_pos_wall[0]][new_pos_wall[1]] != 1 and maze[new_pos[0]][new_pos[1]][1] != 1:
-            maze[new_pos[0]][new_pos[1]] = [shortest_distance(new_pos, end_pos), 0]
+            maze[new_pos[0]][new_pos[1]] = [shortest_distance(new_pos, end_pos), 0,
+                                            maze[start_pos[0]][start_pos[1]][2] + [[new_pos[0], new_pos[1]]]]
             ways.append(maze[new_pos[0]][new_pos[1]])
 
     ways = []
@@ -202,12 +156,11 @@ def a_way_out(maze, start_pos, end_pos, right_path):
     try_move((0, -1), 2)
     shortest_ways = list(filter(lambda x: not x[1], ways))
     shortest_ways.sort(key=lambda x: x[0])
-    if [0, 0] in shortest_ways:
+    if any(sublist[:2] == [0, 0] for sublist in shortest_ways):
         return
     elif shortest_ways:
         new_start = find_element_in_matrix(maze, shortest_ways[0])
-        right_path.append(new_start)
-        a_way_out(maze, new_start, end_pos, right_path)
+        a_way_out(maze, new_start, end_pos)
     else:
         new_start = [1, 1]
         for i in range(1, rows_fixed, 2):
@@ -216,22 +169,39 @@ def a_way_out(maze, start_pos, end_pos, right_path):
                     if maze[i][j][0] < maze[new_start[0]][new_start[1]][0]:
                         new_start = [i, j]
 
-        for _ in range(abs(maze[start_pos[0]][start_pos[1]][0] - maze[new_start[0]][new_start[1]][0]) + 1):
-            right_path.pop()
-        right_path.append(new_start)
-        a_way_out(maze, new_start, end_pos, right_path)
+        a_way_out(maze, new_start, end_pos)
 
 
-path = []
-path.append(list(start))
-solving_maze[start[0]][start[1]] = [shortest_distance(start, end), 0]
-a_way_out(solving_maze, start, end, path)
-path.append(list(end))
-#
-# for row in solving_maze:
-#     print(row)
-#
-# print('\n\n\n')
-# print(path)
-# print('\n\n\n')
+# created_maze = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#                 [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+#                 [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+#                 [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#                 [1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1],
+#                 [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+#                 [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+#                 [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+#                 [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+#                 [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+#                 [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+#                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+created_maze = a
+print('\ngenerated_maze ')
+maze_print(created_maze, cols_fixed, rows_fixed)
+print('\n')
+
+# start and end in fixed format rows, cols
+start = (1, 1)
+end = (13, 13)
+
+solving_maze = copy.copy(created_maze)
+for i in range(1, rows_fixed, 2):
+    for j in range(1, cols_fixed, 2):
+        solving_maze[i][j] = [0, 0, 0]
+
+solving_maze[start[0]][start[1]] = [shortest_distance(start, end), 0, [list(start)]]
+a_way_out(solving_maze, start, end)
+path = solving_maze[13][13][2]
 maze_print_solved(created_maze, cols_fixed, rows_fixed, path)
